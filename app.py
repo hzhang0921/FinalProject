9d0df55c1a455ad3e5855b5937b5888f2309d13b
tylerkero <112517094+tylerkero@users.noreply.github.com>
2022-12-02 17:18:56 -0500

    update

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import redirect
+import requests
+from requests.exceptions import HTTPError
 from workspace import get_puuid
 from flask import request
 from flask import render_template
@@ -7,6 +9,7 @@ from workspace import get_matchid
 from workspace import get_match_stats
 from workspace import get_player_baitPings
 from workspace import get_players
+from workspace import get_match_id
 from workspace import get_player_deaths
 from workspace import get_player_kills
 from workspace import get_player_assists
@@ -23,62 +26,62 @@ from workspace import get_player_CreepScore
 from workspace import get_player_timeSpentDead
 from workspace import get_player_turretTakedowns
 
-
+#initialize flask
 app = Flask(__name__)
-
+#create the index page
 @app.route('/')
 def index():
-
+    #send index to form.html template
     return render_template('form.html')
-
+#create page that displays a users match history
 @app.route('/matchpage/', methods = ["POST"])
 def matchpage():
+    #obtain data from form.html
     form_data = request.form
-    #print(form_data)
-    # print("joen")
-    # print(form_data)
-    # print(form_data.getlist('Summoner Name')[0])
-    # print('keo')
-    #print(form_data[1])
-    #return render_template('matchpage.html')
-        #access the balues from the form
-    # for value in form_data.values():
-    
+    #utilize try to account for error exceptions 
     try:
+        #obtain information from form from immutable dict to string
         summonername = form_data.getlist('Summoner Name')[0]
         tagline = form_data.getlist('Tagline')[0]
+        #obtain puuid from get_puuid function
         puuid = get_puuid(summonername,tagline)
-        #return the template locaiton.html, send in variable sstop and wheelchair
+        #obtian match id from puuid using get_matchid function
         match_id = get_matchid(puuid)
-        print(match_id)
+        #put each match id from list into seperate variables
         match1 = match_id[0]
         match2 = match_id[1]
         match3 = match_id[2]
         match4 = match_id[3]
         match5 = match_id[4]
+        #send variables to matchpage.html to create match history page
         return render_template('matchpage.html', match1= match1, match2=match2, match3=match3, match4=match4, match5 =match5 )
- 
-    # #in the case an error occurs
-    except (IndexError,TypeError):
-    #     #if an index or type error occurs, redirect the user to /error, showing the error.html template
-         return redirect(('/error'))
 
+    #in the case an error occurs
+    except Exception as e:
+    #     #if an error occurs, redirect the user to /error, showing the error.html template
+         return redirect(('/error'))
+#creation of an error page
 @app.route('/error')
 def error():
+    #return error.html template
     return render_template('error.html')
 
+#create app route to display each player in a set match
 @app.route('/match1', methods = ["POST"])
 def match1():
+    #obtain data from matchpage form
     form_data = request.form
-    #print(form_data)
+    #put matchpage data into useable format
     dictmatch_id = form_data.keys()
-    #print(dictmatch_id)
     for value in dictmatch_id:
+        #create match id variable
         matchid=value
-    #print(matchid)
+    #get the data from match using get_match_stats method with input id 
     matchdata = get_match_stats(matchid)
+    #get dictionary of players using get_players
     players = get_players(matchdata)
-    player1 = str(players[0][0])
+    #put each individual player's summoner name into seperate variables
+    player1 = players[0][0]
     player2 = players[1][0]
     player3 = players[2][0]
     player4 = players[3][0]
@@ -88,19 +91,23 @@ def match1():
     player8 = players[7][0]
     player9 = players[8][0]
     player10 = players[9][0]
-
+    #return template to matchplayers.html, send in each player's summoner name in variables
     return render_template('matchplayers.html', player1 = player1, player2 = player2, player3 = player3, player4 = player4, player5 = player5,
     player6= player6, player7= player7, player8= player8, player9 = player9, player10 =player10, matchid = matchid)
     #matchid = 
-
+#create app route from each player's stats
 @app.route('/playerstats', methods =['POST'])
 def matchstats():
+    #obtain data
     form_data = request.form
+    #translate form data into useable format 
     for value in form_data:
         placeholder = value
     newlist = placeholder.split(',')
     matchid = newlist[0]
     summonername =newlist[1]
+    #create variables using functions from workspace.py
+    #variables will be passed to template in the future
     matchdata = get_match_stats(matchid)
     baitpings = get_player_baitPings(matchdata,summonername)
     kills = get_player_kills(matchdata,summonername)
@@ -120,11 +127,10 @@ def matchstats():
     turretTakedowns = get_player_turretTakedowns(matchdata,summonername)
     baitPings = get_player_baitPings(matchdata,summonername)
 
-
-
+    #create the playerstats template which displays a player's stats for a game. Send each variable intialized above through this form
     return render_template('playerstats.html', baitpings=baitpings,summonername=summonername,kills=kills,deaths=deaths, assists=assists, visionScore=visionScore, 
         teamPosition =teamPosition, totaldamage= totaldamage, championName= championName, ObjectiveDamage = ObjectiveDamage, TurretDamage = TurretDamage, goldEarned = goldEarned,
         pinkWards=pinkWards, TotalDamagetoChampions = TotalDamagetoChampions, CreepScore= CreepScore, timeSpentDead = timeSpentDead, turretTakedowns = turretTakedowns, baitPings=baitPings)
-
+#run app
 if __name__ == '__main__':
     app.run(debug=True)    
--- workspace.py
+++ workspace.py
@@ -3,154 +3,187 @@ import json
 import pprint
 from config import RIOTAPI
 import ssl
-
+#allow for url to be analyzed correctly 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-
+#obtain the puuid from a user, using username and tagline
 def get_puuid(username,tagline):
+    #create custom url using username tagline and apikey
     url = 'https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/' + username + '/' + tagline + '?api_key=' + RIOTAPI
+    #decode json file and obtain data
     with urllib.request.urlopen(url) as f:
         repsonse_text = f.read().decode('utf-8')
         data = json.loads(repsonse_text)
-    #pprint.pprint(data)
-    #print (data['puuid'])
+    #return the puuid from the dictionary 'data'
     return data['puuid']
-
+#obtain the matchid using puuid
 def get_matchid(puuid):
+    #create custom url using puuid and api key
     url = 'https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/' + puuid + '/ids?start=0&count=20&api_key=' + RIOTAPI
+    #decode json file
     with urllib.request.urlopen(url) as f:
         repsonse_text = f.read().decode('utf-8')
         data = json.loads(repsonse_text)
-    #pprint.pprint(data)
+    #return data
     return data
 
+#get the overall match stats for each player using match id and api key 
 def get_match_stats(match_id):
     url = 'https://americas.api.riotgames.com/lol/match/v5/matches/' + match_id + '?api_key=' + RIOTAPI
+    #decode json
     with urllib.request.urlopen(url) as f:
         repsonse_text = f.read().decode('utf-8')
         data = json.loads(repsonse_text)
-    #pprint.pprint(data)
+    #return data
     return data
 
+#get the number of kills for a given player using matchdata.
+#Find given player by searching for matching summonername. Return the kills. This is a starter function.
 def get_kills(matchdata):
     playerdict = {}
     for i in range(len(matchdata['info']['participants'])):
         playerdict[matchdata['info']['participants'][i]['summonerName']] = matchdata['info']['participants'][i]['kills']
     return(playerdict)
-
+#return a list of the players in the match by using matchdata dictionary
 def get_players(matchdata):
     playerlist = []
     for i in range(len(matchdata['info']['participants'])):
         playerlist.append([matchdata['info']['participants'][i]['summonerName']])
     return playerlist
 
+#get the number of kills for a given player using matchdata.
+#Find given player by searching for matching summonername. Return the number of baitPings
 def get_player_baitPings(matchdata, summonername):
     for i in range(len(matchdata['info']['participants'])):
         if matchdata['info']['participants'][i]['summonerName']==summonername:
             index = int(i)
     return matchdata['info']['participants'][index]['baitPings']
 
+#get the number of kills for a given player using matchdata.
+#Find given player by searching for matching summonername. Return the number of kills
 def get_player_kills(matchdata, summonername):
-    #print(range(len(matchdata['info']['participants'])))
     for i in range(len(matchdata['info']['participants'])):
         if matchdata['info']['participants'][i]['summonerName']==summonername:
             index = int(i)
-            print(index)
     return matchdata['info']['participants'][index]['kills']
 
+#get the number of deaths for a given player using matchdata.
+#Find given player by searching for matching summonername. Return the number of deaths
 def get_player_deaths(matchdata, summonername):
     for i in range(len(matchdata['info']['participants'])):
         if matchdata['info']['participants'][i]['summonerName']==summonername:
             index = int(i)
     return matchdata['info']['participants'][index]['deaths']
 
+#get the number of assists for a given player using matchdata.
+#Find given player by searching for matching summonername. Return the number of assists
 def get_player_assists(matchdata, summonername):
     for i in range(len(matchdata['info']['participants'])):
         if matchdata['info']['participants'][i]['summonerName']==summonername:
             index = int(i)
     return matchdata['info']['participants'][index]['assists']
 
+#get the Vision Score for a given player using matchdata.
+#Find given player by searching for matching summonername. Return the Vision Score
 def get_player_visionScore(matchdata, summonername):
     for i in range(len(matchdata['info']['participants'])):
         if matchdata['info']['participants'][i]['summonerName']==summonername:
             index = int(i)
     return matchdata['info']['participants'][index]['visionScore']
 
+#get the positoin for a given player using matchdata.
+#Find given player by searching for matching summonername. Return the Position of the Player
 def get_player_teamPosition(matchdata, summonername):
     for i in range(len(matchdata['info']['participants'])):
         if matchdata['info']['participants'][i]['summonerName']==summonername:
             index = int(i)
     return matchdata['info']['participants'][index]['teamPosition']
 
+#get the total damage for a given player using matchdata.
+#Find given player by searching for matching summonername. Return the total damage
 def get_player_totaldamage(matchdata, summonername):
     for i in range(len(matchdata['info']['participants'])):
         if matchdata['info']['participants'][i]['summonerName']==summonername:
             index = int(i)
     return matchdata['info']['participants'][index]['totalDamageDealt']
 
+#get the champion name for a given player using matchdata.
+#Find given player by searching for matching summonername. Return the Champion Name
 def get_player_championName(matchdata, summonername):
     for i in range(len(matchdata['info']['participants'])):
         if matchdata['info']['participants'][i]['summonerName']==summonername:
             index = int(i)
     return matchdata['info']['participants'][index]['championName']
 
+#get the Objective Damage for a given player using matchdata.
+#Find given player by searching for matching summonername. Return the total Objective Damage
 def get_player_ObejctiveDamage(matchdata, summonername):
     for i in range(len(matchdata['info']['participants'])):
         if matchdata['info']['participants'][i]['summonerName']==summonername:
             index = int(i)
     return matchdata['info']['participants'][index]['damageDealtToObjectives']
 
+#get the Turret Damage for a given player using matchdata.
+#Find given player by searching for matching summonername. Return the total Turret Damage
 def get_player_TurretDamage(matchdata, summonername):
     for i in range(len(matchdata['info']['participants'])):
         if matchdata['info']['participants'][i]['summonerName']==summonername:
             index = int(i)
     return matchdata['info']['participants'][index]['damageDealtToTurrets']
 
+#get the Gold Earned for a given player using matchdata.
+#Find given player by searching for matching summonername. Return the total Gold Earned
 def get_player_goldEarned(matchdata, summonername):
     for i in range(len(matchdata['info']['participants'])):
         if matchdata['info']['participants'][i]['summonerName']==summonername:
             index = int(i)
     return matchdata['info']['participants'][index]['goldEarned']
 
+#get the number of pinkWards purchased for a given player using matchdata.
+#Find given player by searching for matching summonername. Return the total number of Pink Wards
 def get_player_pinkWards(matchdata, summonername):
     for i in range(len(matchdata['info']['participants'])):
         if matchdata['info']['participants'][i]['summonerName']==summonername:
             index = int(i)
     return matchdata['info']['participants'][index]['sightWardsBoughtInGame']
 
+#get the Total Damage Dealt to other players for a given player using matchdata.
+#Find given player by searching for matching summonername. Return the total Damage to Champions
 def get_player_TotalDamagetoChampions(matchdata, summonername):
     for i in range(len(matchdata['info']['participants'])):
         if matchdata['info']['participants'][i]['summonerName']==summonername:
             index = int(i)
     return matchdata['info']['participants'][index]['totalDamageDealtToChampions']
 
+#get the Creep Score for a given player using matchdata.
+#Find given player by searching for matching summonername. Return the total Creep Score
 def get_player_CreepScore(matchdata, summonername):
     for i in range(len(matchdata['info']['participants'])):
         if matchdata['info']['participants'][i]['summonerName']==summonername:
             index = int(i)
     return matchdata['info']['participants'][index]['totalMinionsKilled']
 
+#get the Time Spent Dead for a given player using matchdata.
+#Find given player by searching for matching summonername. Return the total Time Spent Dead
 def get_player_timeSpentDead(matchdata, summonername):
     for i in range(len(matchdata['info']['participants'])):
         if matchdata['info']['participants'][i]['summonerName']==summonername:
             index = int(i)
     return matchdata['info']['participants'][index]['totalTimeSpentDead']
 
+#get the number of Turret Takedowns for a given player using matchdata.
+#Find given player by searching for matching summonername. Return the total Turret Takedowns
 def get_player_turretTakedowns(matchdata, summonername):
     for i in range(len(matchdata['info']['participants'])):
         if matchdata['info']['participants'][i]['summonerName']==summonername:
             index = int(i)
     return matchdata['info']['participants'][index]['turretTakedowns']
 
+#return the match id using matchdata as an input
+def get_match_id(matchdata):
+    return matchdata['metadata']['matchId']
 
-def full_stats(summonername,tagline):
-    puuid = get_puuid(summonername,tagline)
-    match1 = get_matchid(puuid)[0]
-    matchstats = get_match_stats(match1)
-    kills = get_kills(matchstats)
-    return kills
-
-
+#testing
 def main():
     
    # get_puuid('SchtankyLeg','PANTS')
@@ -167,10 +200,10 @@ def main():
     # print(get_player_championName('QuickPlatinum', 'NA1_4504131447'))
     #print(get_player_baitPings(get_match_stats(get_matchid(get_puuid('QuickPlatinum','NA1'))[0])))
     #print(get_puuid('QuickPlatinum','NA1'))
-    print(get_player_kills("QuickPlatinum", 'NA1_4504131447'))
+    #print(get_player_kills("QuickPlatinum", 'NA1_4504131447'))
     #print(get_player_stats(match,'QuickPlatinum'))
     #get_kills(get_match_stats(get_matchid(get_puuid('QuickPlatinum','NA1'))[0]))
     pass
-
+#run program
 if __name__== '__main__':
     print(main())
